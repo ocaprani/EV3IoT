b23b9e198f2b898e75d7e6846a519897bad8cab6
ocaprani <37537933+ocaprani@users.noreply.github.com>
2020-08-20 21:19:54 +0200

    Add files via upload

---
+++ GetRequestHTTP/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+def http_get(url):
+    import socket
+    _, _, host, path = url.split('/', 3)
+    addr = socket.getaddrinfo(host, 80)[0][-1]
+    s = socket.socket()
+    s.connect(addr)
+    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+    while True:
+        data = s.recv(100)
+        if data:
+            print(str(data, 'utf8'), end='')
+        else:
+            break
+    s.close()
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+# Write your program here.
+ev3.speaker.beep()
+http_get('http://micropython.org/ks/test.html')
